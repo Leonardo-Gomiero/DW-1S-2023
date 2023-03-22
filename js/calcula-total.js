2b49faf02eacef016e296b4d74930a809864d714
Leonardo Gomiero Capela Capoccia <93843916+Leonardo-Gomiero@users.noreply.github.com>
2023-03-21 22:00:04 -0300

    Add files via upload

---
+++ js/calcula-total.js
@@ -0,0 +1,39 @@
+var titulo = document.querySelector(".titulo");
+var titulo2 = document.querySelector(".titulo2");
+titulo.textContent = "Lista de Encomendas";
+titulo2.textContent = "Adicionar nova encomenda";
+
+var botao = document.querySelector("#id-botao");
+
+
+
+//Obtendo os valores dos campos:
+var nome = document.querySelector("#nome");
+var quantidadeSel = document.querySelector("#quantit");
+var produto = document.querySelector("#produto");
+var valorUni = document.querySelector("#value");
+
+//Captura os dados do cliente
+var clientes = document.querySelectorAll(".cliente");
+var totais = document.querySelectorAll(".total");
+
+
+
+console.log(clientes);
+
+for (var i = 0; i < clientes.length; i++) {
+    console.log("Quantidade:(" + i + "): "  + clientes[i].querySelector(".qtde").textContent);
+    console.log("Valor(" + i + "): "  + clientes[i].querySelector(".valor").textContent);
+
+    var quantidade = clientes[i].querySelector(".qtde").textContent;
+    var valor = clientes[i].querySelector(".valor").textContent;
+
+    valor *= 1;
+
+    clientes[i].querySelector(".valor").textContent = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    var total = quantidade * valor;
+
+    clientes[i].querySelector(".total").textContent = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    console.log("TOTAL: " + total);     
+}
---
+++ js/form.js
@@ -0,0 +1,62 @@
+/*============================
+    EVENTOS
+============================*/
+
+//O evento a seguir recebe os valores do forms e adiciona na tabela
+botao.addEventListener("click", function (event) {
+
+
+    event.preventDefault();
+
+    //Armazena o formulário em uma variável
+    var form = document.querySelector("#formulario");
+
+    //Armazena a tabela em uma variável
+    var tabela = document.querySelector("#tabela");
+
+    tabela.appendChild(montaTr(obtemEncomenda(form)));
+
+
+})
+
+
+function obtemEncomenda(form) {
+    var encomenda = {
+        nome: form.nome.value,
+        produto: form.produto.value,
+        qtde: form.quantit.value,
+        unitario: form.value.value,
+    }
+    return encomenda;
+}
+
+function montaTr(encomenda) {
+
+    //Criando uma linha que será adicionada na tabela
+    const linha = document.createElement('tr');
+
+    //Criando colunas que serão adicionadas à linha criada acima
+    const colNome = montaTd(encomenda.nome, 'nome');
+    const colProduto = montaTd(encomenda.produto, 'produto');
+    const colQuantidade = montaTd(encomenda.qtde, 'quantit');
+    const colValor = montaTd(encomenda.unitario, 'value');
+    const colTotal = montaTd(0, 'total');
+
+    //Adicionando as colunas à linha
+    encomendaTr.appendChild(colNome);
+    encomendaTr.appendChild(colProduto);
+    encomendaTr.appendChild(colQuantidade);
+    encomendaTr.appendChild(colValor);
+    encomendaTr.appendChild(colTotal);
+
+    return encomendaTr;
+
+}
+
+function montaTd(dados, classe) {
+    var td = document.createElement('td');
+    td.textContent = dados;
+    td.classList.add(classe);
+
+    return td;
+}
