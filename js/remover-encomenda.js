35381fe0b44b3cc1c55f86d35b4c8769986fba28
Leonardo Gomiero Capela Capoccia <93843916+Leonardo-Gomiero@users.noreply.github.com>
2023-04-04 22:23:24 -0300

    Add files via upload

---
+++ js/remover-encomenda.js
@@ -0,0 +1,18 @@
+var tabela = document.querySelector("table");
+
+
+tabela.addEventListener("dblclick", function (event) {
+
+    console.log(event.target);
+
+    //A linha que sofreu o duplo clique sera removida:
+    //this.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 600);
+    
+
+    //event.target.parentNode.remove();
+});
