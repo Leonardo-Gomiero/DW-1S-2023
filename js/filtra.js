07bf1cc5b718a3c2faa27365a836008ebcef1b54
Leonardo Gomiero Capela Capoccia <93843916+Leonardo-Gomiero@users.noreply.github.com>
2023-04-11 22:06:27 -0300

    Add files via upload

--- js/filtra.js
+++ js/filtra.js
@@ -1,21 +1,37 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input", function(){
+campoFiltro.addEventListener("input", function () {
     //console.log(this.value);
     var clientes = document.querySelectorAll(".cliente");
 
-    for (var i = 0; i < clientes.length; i++){
-        var cliente = clientes[i];
-        var tdNome = cliente.querySelector(".nome");
-        var nome = tdNome.textContent;
+    if (this.value.length > 0) {
+        for (var i = 0; i < clientes.length; i++) {
+            var cliente = clientes[i];
+            var tdNome = cliente.querySelector(".nome");
+            var nome = tdNome.textContent;
 
-        if(nome != this.value){
-            //Não é igual, oculta linha
-            cliente.classList.add("invisivel");
 
-        }else{
-            //É igual, exibe a linha
+            //Busca por Expressão Regular
+            var expressao = new RegExp(this.value, "i");
+
+
+            //if (nome != this.value) {
+             
+            //Expressao Regular do JS
+            if(!expressao.test(nome)){
+                //Não é igual, oculta linha
+                cliente.classList.add("invisivel");
+
+            } else {
+                //É igual, exibe a linha
+                cliente.classList.remove("invisivel");
+            }
+        }
+    }else{
+        for (var i = 0; i < clientes.length; i++) {
+            var cliente = clientes[i];
             cliente.classList.remove("invisivel");
         }
     }
+
 })
